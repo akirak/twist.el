08011bbc8efc1e071a1f09e656573385fdc1eda6
Akira Komamura <akira.komamura@gmail.com>
2022-01-15 21:04:11 +0900

    chore: Update lock files

--- lock/flake.nix
+++ lock/flake.nix
@@ -8,10 +8,22 @@
       repo = "dash.el";
       type = "github";
     };
+    elsa = {
+      flake = false;
+      owner = "emacs-elsa";
+      repo = "Elsa";
+      type = "github";
+    };
     elx = {
       flake = false;
       owner = "emacscollective";
       repo = "elx";
+      type = "github";
+    };
+    f = {
+      flake = false;
+      owner = "rejeep";
+      repo = "f.el";
       type = "github";
     };
     magit-section = {
@@ -26,16 +38,40 @@
       repo = "nix-mode";
       type = "github";
     };
-    transient = {
+    package-lint = {
       flake = false;
-      owner = "magit";
-      repo = "transient";
+      owner = "purcell";
+      repo = "package-lint";
+      type = "github";
+    };
+    relint = {
+      flake = false;
+      owner = "mattiase";
+      repo = "relint";
+      type = "github";
+    };
+    s = {
+      flake = false;
+      owner = "magnars";
+      repo = "s.el";
+      type = "github";
+    };
+    trinary = {
+      flake = false;
+      owner = "emacs-elsa";
+      repo = "trinary-logic";
       type = "github";
     };
     twist = {
       flake = false;
       owner = "akirak";
       repo = "twist.el";
+      type = "github";
+    };
+    xr = {
+      flake = false;
+      owner = "mattiase";
+      repo = "xr";
       type = "github";
     };
   };
